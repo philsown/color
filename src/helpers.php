0a6e55687f70101d7c31e32920e41f9d6d893991
Phillip Harrington <phharrin@cisco.com>
2017-09-14 18:56:27 -0400

    adding helpers

---
+++ src/helpers.php
@@ -0,0 +1,36 @@
+<?php
+
+if (function_exists('ColorColorCall')) {
+    function ColorColorCall($method, $color, $p)
+    {
+        return (Color\Color::create($color))->$method($p);
+    }
+}
+
+if (function_exists('lighten')) {
+    function lighten($color, $p)
+    {
+        return $ColorColorCall('lighten', $color, $p);
+    }
+}
+
+if (function_exists('darken')) {
+    function darken($color, $p)
+    {
+        return $ColorColorCall('darken', $color, $p);
+    }
+}
+
+if (function_exists('saturate')) {
+    function saturate($color, $p)
+    {
+        return $ColorColorCall('saturate', $color, $p);
+    }
+}
+
+if (function_exists('desaturate')) {
+    function desaturate($color, $p)
+    {
+        return $ColorColorCall('desaturate', $color, $p);
+    }
+}
